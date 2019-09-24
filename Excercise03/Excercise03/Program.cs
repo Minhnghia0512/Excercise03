5b7ade5a53cb64faa3c03bf53845b9f6d138c497
nghia0512 <okthiisme@gmail.com>
2019-09-24 14:55:12 +0700

    yareyare

--- Excercise03/Excercise03/Program.cs
+++ Excercise03/Excercise03/Program.cs
@@ -11,12 +11,19 @@ namespace Excercise03
         static void Main(string[] args)
         {
             Console.WriteLine("chao the gioi");
-            Hline(10);
+            Hline(10, '*'); Console.WriteLine();
+            Vline(10, 'a'); Console.WriteLine();
+            Hline(10, 'b'); Console.WriteLine();
         }
-        static void Hline(int n)
+        static void Hline(int n, char c)
         {
             for (int i = 0; i < n; i++)
-                Console.Write("*");
+                Console.Write(c);
+        }
+        static void Vline(int n, char c)
+        {
+            for (int i = 0; i < n; i++)
+                Console.WriteLine(c);
         }
     }
 }
