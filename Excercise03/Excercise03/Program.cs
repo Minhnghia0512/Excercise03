fed0c76f8c7a87f25bed1e9277a74e0dc4341911
nghia0512 <okthiisme@gmail.com>
2019-09-24 14:43:33 +0700

    yareyare

--- Excercise03/Excercise03/Program.cs
+++ Excercise03/Excercise03/Program.cs
@@ -11,6 +11,12 @@ namespace Excercise03
         static void Main(string[] args)
         {
             Console.WriteLine("chao the gioi");
+            Hline(10);
+        }
+        static void Hline(int n)
+        {
+            for (int i = 0; i < n; i++)
+                Console.Write("*");
         }
     }
 }
